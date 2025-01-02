9a6c955438a37d53271eb28e3a33ad13ef1d199c
Ayah-Ayyash <ayahayyash710@gmail.com>
2025-01-02 05:02:42 +0200

    29

---
+++ Assets/Assignments/Assignment29/UnitySpecificScript.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UnitySpecificScript : MonoBehaviour
+{
+    GameObject TargetObject;
+    GameObject JokerObject;
+    Light LLight;
+    void OnEnable()
+    {
+        print("GameObject Enabled");
+    }
+
+ void OnDisable()
+    {
+        print("GameObject Disabled");
+    }
+    void Start()
+    {
+        print("Game started");
+        //
+        TargetObject = GameObject.Find("TargetObject");
+        if (TargetObject != null)
+        {
+            print($"Found object by name:{TargetObject.name}");
+
+        }
+        else
+        {
+            print("No TargetObject  found");
+        }
+        //
+        JokerObject = GameObject.FindGameObjectWithTag("Joker");
+
+        if (JokerObject != null)
+        {
+            print($"Found object by tag: {JokerObject.name}");
+        }
+        else
+        {
+            print("No Joker object found");
+        }
+        //
+        LLight = GameObject.FindObjectOfType<Light>();
+        if (LLight != null)
+        {
+            print($"Found object of type Light: {LLight.name}");
+        }
+        else
+        {
+            print("No Light object found");
+        }
+
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (TargetObject != null)
+            {
+                TargetObject?.SetActive(false);
+                print("TargetObject deactivated!");
+            }
+            else
+            {
+                print("TargetObject is null ");
+            }
+        }
+    }
+
+   
+}
+}
