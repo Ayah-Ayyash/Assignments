5cb18543521ba5cf846de691cf91f60e10b630fa
Ayah-Ayyash <ayahayyash710@gmail.com>
2025-01-19 17:57:01 +0200

    twosum

--- Assets/Assignments/twosum.cs
+++ Assets/Assignments/twosum.cs
@@ -1,14 +1,13 @@
-public class twosum {
-   
-    public int[] TwoSum(int[] nums, int target) {
-        for(int i = 0; i<nums.Length ; i++){
-             for(int j = 0; j<nums.Length ; j++){
-                if(nums[i] + nums[j]==target){
-                    return new int[]{i,j};
-                }
-             }
-        }
-        
-    }
 
+public class TwoSum {
+    public int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = i + 1; j < nums.Length; j++) { 
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] {}; 
+    }
 }
