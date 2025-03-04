b378f1f576195a8ed6984b0a4ef69623db91c1f8
Ayah-Ayyash <ayahayyash710@gmail.com>
2025-03-04 16:12:52 +0200

    Random

---
+++ Assets/script/Kinetic/Random.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+public class Random
+{
+    private Dictionary<int, int> dictionary;
+    private List<int> list;
+    private Random random;
+
+    public Random()
+    {
+        dictionary = new Dictionary<int, int>();
+        list = new List<int>();
+        random = new Random();
+    }
+
+    public bool Insert(int val)
+    {
+        if (dictionary.ContainsKey(val)) return false;
+        dictionary[val] = list.Count;
+        list.Add(val);
+        return true;
+    }
+
+    public bool Remove(int val)
+    {
+        if (!dictionary.ContainsKey(val)) return false;
+
+        int index = dictionary[val];
+        int lastElement = list[list.Count - 1];
+
+        list[index] = lastElement;
+        dictionary[lastElement] = index;
+
+        list.RemoveAt(list.Count - 1);
+        dictionary.Remove(val);
+
+        return true;
+    }
+
+    public int GetRandom()
+    {
+        int randomIndex = random.Next(list.Count);
+        return list[randomIndex];
+    }
+}
